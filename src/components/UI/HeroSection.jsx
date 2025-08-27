00cd66ae9921cd40b0a11a6c1f230a224b2b3aa9
code-withshivani <theshivani0519@gmail.com>
2025-08-27 17:14:30 +0530

    Update HeroSection.jsx

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,7 +1,18 @@
- import { FaArrowRight } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 import {NavLink} from "react-router-dom";
 export const HeroSection = () => {
+ 
+const[showPopUp ,setShowPopUp] = useState(false);
 
+useEffect(() =>{
+  const time =setTimeout(() =>{
+    setShowPopUp(true);},5000);
+    return() => clearTimeout(time);
+},[]);
+
+
+ 
   return(
     <main className="hero-section main">
     <div className="container grid gird-two-cols" style={{display:"grid",
@@ -22,6 +33,34 @@ export const HeroSection = () => {
         " className="banner-image" />
       </div>
     </div> 
+
+{showPopUp && (
+        <div 
+        style={{
+        position:"fixed",
+        top:"17%",
+        left:"50%",
+        transform:"translate(-50%,-50%)",
+        background:"white",
+        padding:"20px",
+        borderRadius:"10px",
+        boxShadow:"0px 0px 10px rgba(0,0,0,3)",
+        zIndex:"999" ,
+        }}
+        >
+        <h2 style={{ color: "#030303ff" }}>Welcome to My Website 🌍</h2>
+          <p style={{ color: "#030303ff" }}>
+            Start exploring countries and learn more!
+          </p>
+          <button
+            style={{ marginTop: "10px" }}
+            onClick={() => setShowPopUp(false)}
+          >
+            Close
+          </button>
+        </div>
+      )};
+     
   </main>
   )
 
