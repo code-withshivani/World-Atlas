3a4ed80a47d02737f9d2284e5caf89fbd7a6d408
code-withshivani <theshivani0519@gmail.com>
2025-08-27 15:37:05 +0530

    Update Country.jsx

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState, useTransition } from "react";
 import  {getCountryData}  from "../api/postApi";
 
 import { CountryCard } from "../components/Layout/CountryCard"; 
+import { SearchFilter } from "../components/UI/SearchFilter";
 
  export const Country = () => {
    
 const[isPending, startTransition] = useTransition();
 const [countries,setCountries] = useState([]);
 
+const [search, setSearch] = useState();
+const [filter, setFilter] = useState("all");
+  
 useEffect(() => {
 
 startTransition( async () => {
@@ -19,9 +23,34 @@ startTransition( async () => {
 
  if(isPending) return  <h1>Loading...</h1> ;
 
+const searchCountry = (country) => {
+  if(search){
+   return country.name.common.toLowerCase().includes(search.toLowerCase());
+  }
+  return country;
+}
+
+const filterRegion = (country) => {
+
+if(filter === "all") return country;
+
+return country.region === filter;
+
+}
+
+ const filterCountries = countries.filter((country) => searchCountry(country) && filterRegion(country));
+  
    return (
       
       <section className="country-section">
+       <SearchFilter 
+      search={search}
+      setSearch={setSearch}
+      filter={filter}
+      setFilter={setFilter}
+      countries = {countries}
+      setCountries={setCountries}
+      />
        <ul className="grid grid-four-cols">{
          
         countries.map((curCountry ,index ) => {
